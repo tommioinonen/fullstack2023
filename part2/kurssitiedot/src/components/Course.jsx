5546f27bb577cb2f68d104e0ef713c588f90bde1
Tommi Oinonen <tommirj.oinonen@gmail.com>
2024-06-03 13:27:31 +0300

    exercise 2.1

---
+++ part2/kurssitiedot/src/components/Course.jsx
@@ -0,0 +1,33 @@
+
+const Part = ({ part }) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  )
+}
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map(part => <Part key={part.id} part={part} />)}
+    </div>
+  )
+}
+
+const Header = ({ name }) => {
+  return (
+    <h1>{name}</h1>
+  )
+}
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+    </div>
+  )
+}
+
+export default Course
